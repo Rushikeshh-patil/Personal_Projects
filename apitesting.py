a9dfcd2c031fab608e4083c4c0413b20ad586f0f
Rushikeshh-patil <coolrushipatil1995@gmail.com>
2020-06-18 20:19:36 -0400

    test

---
+++ apitesting.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from urllib.request import urlopen
+import json 
+
+
+url = 'https://data.cityofnewyork.us/resource/ic3t-wcy2.json?borough=BRONX&job_type=A1&job_status=P&house__=2952'
+api_key = '8e1opzbvvmfbj33klbdhflgfu'
+api_secret = '2hypbqgitvabt0ux3cu4wtiawwbhsze67u2q914qz1xmci5gkj'
+app_token = 'JRzXhDCoskeaC7oyCwEny9HRX'
+
+usefulfile = urlopen(url)
+data = json.load(usefulfile)
+
+print (data[0]['job__'])
+print()
+
+
+
+
+
+
